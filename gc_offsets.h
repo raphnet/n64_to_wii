84322ec539c0d81b8559b19d3d36249bc4714c66
Raphaël Assénat <raph@raphnet.net>
2007-10-07 21:00:41 +0000

    bit offsets for buttons and axes

---
+++ gc_offsets.h
@@ -0,0 +1,25 @@
+#ifndef _GC_OFFSET_H__
+#define _GC_OFFSET_H__
+
+#define GC_OFF_START	3
+#define GC_OFF_Y		4
+#define GC_OFF_X		5
+#define GC_OFF_B		6
+#define GC_OFF_A		7
+#define GC_OFF_L		9
+#define GC_OFF_R		10
+#define GC_OFF_Z		11
+#define GC_OFF_D_UP		12
+#define GC_OFF_D_DOWN	13
+#define GC_OFF_D_RIGHT	14
+#define GC_OFF_D_LEFT	15
+
+#define GC_OFF_JOY_X	16
+#define GC_OFF_JOY_Y	24
+#define GC_OFF_CX		32
+#define GC_OFF_CY		40
+#define GC_OFF_L_VAL	48
+#define GC_OFF_R_VAL	56
+
+#endif // _GC_OFFSET_H__
+
---
+++ n64_offsets.h
@@ -0,0 +1,19 @@
+#ifndef _n64_offsets_h__
+#define _n64_offsets_h__
+
+#define N64_OFF_A		0
+#define N64_OFF_B		1
+#define N64_OFF_Z		2
+#define N64_OFF_START	3
+#define N64_OFF_D_UP	4
+#define N64_OFF_D_DOWN	5
+#define N64_OFF_D_LEFT	6
+#define N64_OFF_D_RIGHT	7
+#define N64_OFF_L		10
+#define N64_OFF_R		11
+#define N64_OFF_C_UP	12
+#define N64_OFF_C_DOWN	13
+#define N64_OFF_C_LEFT	14
+#define N64_OFF_C_RIGHT	15
+
+#endif // _n64_offsets_h__
